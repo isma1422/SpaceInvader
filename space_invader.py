ef6e109e1cfa85d62f33a4ac60bf9be876278529
isma1422 <isma.elharry@gmail.com>
2018-05-05 18:56:35 -0300

    Initial commit

---
+++ space_invader.py
@@ -0,0 +1,38 @@
+import sys
+import pygame
+from ship import Ship
+from settings import Settings
+from pygame.sprite import Group
+import game_functions as gf
+
+def run_game():
+	#Initialize game and create a screen object
+	pygame.init()
+	ai_settings = Settings()
+	
+	screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
+	pygame.display.set_caption("Alien invasion")
+		
+	
+	#Make a ship
+	ship = Ship(screen)
+			
+	#Make a group to store bullets in.
+	bullets = Group()
+	
+	#Group of aliens
+	aliens = Group()
+	
+	gf.create_fleet(ai_settings, screen, ship, aliens)
+	
+	#Start the main loop for the game
+	while True:
+		
+		gf.check_events(ai_settings, screen, ship, bullets)
+		ship.update()
+		gf.update_bullets(aliens, bullets)	
+		gf.update_aliens(ai_settings, aliens)
+		gf.update_screen(ai_settings,screen,ship, aliens, bullets)
+		
+
+run_game()
